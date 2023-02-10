65102cce7c0a80c37f4382bd10689c704c1c4598
productdevbook <hi@productdevbook.com>
2023-02-10 07:00:42 +0300

    fix: index export

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import { EmergencyEntity, UserEntity } from './entity'
 export { UserEntity, EmergencyEntity }
+export type { ResultInterface } from './types'
+export type { Shared, Storage } from './repo'
+
+export { DatabaseSeeder } from './seeders/DatabaseSeeder'
+export { createStorage } from './repo/index.js'
+export { createMikroORMPostgress } from './db/create.js'
 
 export const entities = [UserEntity, EmergencyEntity]
-export type { ResultInterface } from './types'
-export { createStorage } from './repo'
-export type { Shared, Storage } from './repo'
-export { DatabaseSeeder } from './seeders/DatabaseSeeder'
-export { createMikroORMPostgress } from './db/create'
-
-const data = 'hello'
-
-export { data }
