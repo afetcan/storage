a8c74122ccc61bdc65b3465cc61753bba86e84a1
productdevbook <hi@productdevbook.com>
2023-02-10 07:08:53 +0300

    fix: export createMikroORMPostgress

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { EmergencyEntity, UserEntity } from './entity'
+import { createMikroORMPostgress } from './db/create.js'
+
 export { UserEntity, EmergencyEntity }
 export type { ResultInterface } from './types'
 export type { Shared, Storage } from './repo'
 
 export { DatabaseSeeder } from './seeders/DatabaseSeeder'
 export { createStorage } from './repo/index.js'
-export { createMikroORMPostgress } from './db/create.js'
+export { createMikroORMPostgress }
 
 export const entities = [UserEntity, EmergencyEntity]
